73c22beb79280813f8181af9a315402d10fafea5
chengxiaolei <cxlhk1988@163.com>
2017-09-09 01:22:42 +0800

    map  test

--- src/main/java/com/snowstone/commons/kern/apiext/collection/MapUtil.java
+++ src/main/java/com/snowstone/commons/kern/apiext/collection/MapUtil.java
@@ -8,77 +8,81 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.commons.beanutils.BeanUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import com.snowstone.commons.kern.apiext.auth.BASE64;
 
 public class MapUtil {
+	private final static Logger logger = LoggerFactory.getLogger(MapUtil.class);
 
-	// Map --> Bean 2: 利用org.apache.commons.beanutils 工具类实现 Map --> Bean  
-    public static void transMap2Bean2(Map<String, Object> map, Object obj) {  
-        if (map == null || obj == null) {  
-            return;  
-        }  
-        try {  
-            BeanUtils.populate(obj, map);  
-        } catch (Exception e) {  
-            System.out.println("transMap2Bean2 Error " + e);  
-        }  
-    }  
-	
-    // Map --> Bean 1: 利用Introspector,PropertyDescriptor实现 Map --> Bean  
-    public static void transMap2Bean(Map<String, Object> map, Object obj) {  
-  
-        try {  
-            BeanInfo beanInfo = Introspector.getBeanInfo(obj.getClass());  
-            PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();  
-  
-            for (PropertyDescriptor property : propertyDescriptors) {  
-                String key = property.getName();  
-  
-                if (map.containsKey(key)) {  
-                    Object value = map.get(key);  
-                    // 得到property对应的setter方法  
-                    Method setter = property.getWriteMethod();  
-                    setter.invoke(obj, value);  
-                }  
-  
-            }  
-  
-        } catch (Exception e) {  
-            System.out.println("transMap2Bean Error " + e);  
-        }  
-  
-        return;  
-  
-    }  
-    
- // Bean --> Map 1: 利用Introspector和PropertyDescriptor 将Bean --> Map  
-    public static Map<String, Object> transBean2Map(Object obj) {  
-  
-        if(obj == null){  
-            return null;  
-        }          
-        Map<String, Object> map = new HashMap<String, Object>();  
-        try {  
-            BeanInfo beanInfo = Introspector.getBeanInfo(obj.getClass());  
-            PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();  
-            for (PropertyDescriptor property : propertyDescriptors) {  
-                String key = property.getName();  
-  
-                // 过滤class属性  
-                if (!key.equals("class")) {  
-                    // 得到property对应的getter方法  
-                    Method getter = property.getReadMethod();  
-                    Object value = getter.invoke(obj);  
-  
-                    map.put(key, value);  
-                }  
-  
-            }  
-        } catch (Exception e) {  
-            System.out.println("transBean2Map Error " + e);  
-        }  
-  
-        return map;  
-  
-    }  
+	// Map --> Bean 2: 利用org.apache.commons.beanutils 工具类实现 Map --> Bean
+	public static void transMap2Bean2(Map<String, Object> map, Object obj) {
+		if (map == null || obj == null) {
+			return;
+		}
+		try {
+			BeanUtils.populate(obj, map);
+		} catch (Exception e) {
+			logger.error("transMap2Bean2转换失败", e);
+		}
+	}
+
+	// Map --> Bean 1: 利用Introspector,PropertyDescriptor实现 Map --> Bean
+	public static void transMap2Bean(Map<String, Object> map, Object obj) {
+
+		try {
+			BeanInfo beanInfo = Introspector.getBeanInfo(obj.getClass());
+			PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
+
+			for (PropertyDescriptor property : propertyDescriptors) {
+				String key = property.getName();
+
+				if (map.containsKey(key)) {
+					Object value = map.get(key);
+					// 得到property对应的setter方法
+					Method setter = property.getWriteMethod();
+					setter.invoke(obj, value);
+				}
+
+			}
+
+		} catch (Exception e) {
+			logger.error("transMap2Bean Error",e);
+		}
+
+		return;
+
+	}
+
+	// Bean --> Map 1: 利用Introspector和PropertyDescriptor 将Bean --> Map
+	public static Map<String, Object> transBean2Map(Object obj) {
+
+		if (obj == null) {
+			return null;
+		}
+		Map<String, Object> map = new HashMap<String, Object>();
+		try {
+			BeanInfo beanInfo = Introspector.getBeanInfo(obj.getClass());
+			PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
+			for (PropertyDescriptor property : propertyDescriptors) {
+				String key = property.getName();
+
+				// 过滤class属性
+				if (!key.equals("class")) {
+					// 得到property对应的getter方法
+					Method getter = property.getReadMethod();
+					Object value = getter.invoke(obj);
+
+					map.put(key, value);
+				}
+
+			}
+		} catch (Exception e) {
+			logger.error("transBean2Map Error",e);
+		}
+
+		return map;
+
+	}
 }
--- src/test/java/com/vshcxl/common/collection/MapTest.java
+++ src/test/java/com/vshcxl/common/collection/MapTest.java
@@ -10,8 +10,8 @@ import com.snowstone.commons.kern.apiext.collection.MapUtil;
 
 public class MapTest {
 
-	@Test
-	public void convertMap() throws InstantiationException, IllegalAccessException {
+	@Test//ok
+	public void transMap2Bean() throws InstantiationException, IllegalAccessException {
 
 		Map map = new HashMap<>();
 		map.put("name", "zhangsan");
@@ -20,6 +20,43 @@ public class MapTest {
 		Person per = new Person();
 		MapUtil.transMap2Bean(map, per);
 		System.out.println(per.getAge() + " " + per.getName());
-
+		
 	}
+	
+	
+	@Test//ok
+	public void transMap2Bean2() throws InstantiationException, IllegalAccessException {
+
+		Map map = new HashMap<>();
+		map.put("name", "zhangsan");
+		map.put("age", "25");
+
+		Person per = new Person();
+		MapUtil.transMap2Bean2(map, per);
+		System.out.println(per.getAge() + " " + per.getName());
+	}
+	
+	@Test//ok
+	public void transBean2Map() throws InstantiationException, IllegalAccessException {
+
+		Map<String, Object> map = new HashMap<>();
+
+		Person per = new Person();
+		per.setAge("22");
+		per.setName("lisi");
+		
+		map=MapUtil.transBean2Map(per);
+		System.out.println(map.get("age")+"  "+per.getName() );
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
